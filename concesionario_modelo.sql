7e9d4dbea1f827b5b65cc64d56a6bff5fa7673a8
fragiacomo <166665871+fragiacomo@users.noreply.github.com>
2024-11-06 18:35:31 -0300

    Add files via upload

--- concesionario_modelo.sql
+++ concesionario_modelo.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `concesionario2` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `concesionario2`;
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
 -- Host: localhost    Database: concesionario
@@ -50,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-31 20:20:28
+-- Dump completed on 2024-11-06 17:51:06
